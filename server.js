381413292aec422a3172b15015afe3a53d110561
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-09-11 09:37:19 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
   
 const express = require("express");
-const router = express.Router();
 const helmet = require("helmet");
 const path = require("path");
 require("dotenv").config();
@@ -13,16 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-router.post("/api", (req, res)=> {
-  try {
-    console.log(req)
-  res.json(req)
-  } catch (error) {
-    console.log(error)
-    res.json(error)
-  }
-  
-});
+app.use("/api", require("./routes/api/user"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
